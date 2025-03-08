e448062be4f1215188281f10d49c073b6cebcca3
adrianjafe <145451036+adrianjafe@users.noreply.github.com>
2025-03-08 12:47:18 +0100

    Actualizacion de la clase Program.cs

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -10,10 +10,26 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
-
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            Console.WriteLine("Bienvenido al programa de las torres de Hanoi de Alex y Adrian.\n");
+            Console.WriteLine("Para empezar, cuantos discos quieres que tenga las torres de Hanoi:");
+            ConsoleKeyInfo num_disco = Console.ReadKey();
+            Console.WriteLine("\nMuy bien. Ahora dime que clase de metodo quieres utilizar para resolver las torres, Recursivo o Iterativo(Indicalo con la la R para recursivo y I para iterativo):");
+            char metodo = char.ToLower(Console.ReadKey().KeyChar);
+            switch (metodo)
+            {
+                case 'r':
+                    Console.WriteLine("\nPerfecto, pues empezamos!!!\n");
+                    Console.WriteLine("Has escogido el metodo recursivo.\n");
+                    break;
+                case 'i':
+                    Console.WriteLine("\nPerfecto, pues empezamos!!!\n");
+                    Console.WriteLine("Has escogido el metodo iterativo.\n");
+                    break;
+                default:
+                    Console.WriteLine("\nNo has seleccionado ningun tipo de metodo.");
+                    break;
+            }
+            
         }
     }
 }
